58ab30a7ebce94dfc47363f98d630e867fca7c20
sandydoo <hey@sandydoo.me>
2020-12-30 04:43:04 +0300

    Exclude private IPs from the tunnel

--- ProcessConfigs.hs
+++ ProcessConfigs.hs
@@ -2,7 +2,7 @@
 {- stack script
     --ghc-options "-Wall"
     --resolver lts-16.15
-    --package aeson,bytestring,containers,directory,filepath,http-conduit,lens,lens-aeson,text,utf8-string,zip
+    --package aeson,bytestring,containers,directory,filepath,http-conduit,iproute,lens,lens-aeson,text,utf8-string,zip
 -}
 
 
@@ -15,6 +15,7 @@ import Data.Aeson.Lens
 import Data.Aeson.TH
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as UTF8
+import Data.IP (IPRange)
 import qualified Data.Map as Map
 import           Data.Map (Map)
 import Data.Maybe (isJust, fromMaybe)
@@ -136,8 +137,31 @@ isPreferredCountryCode code = preferredCountryCodes ^? ix code
 -- Generate config
 
 
+publicIPRanges :: Set IPRange
+publicIPRanges = Set.fromList [ "1.0.0.0/8", "2.0.0.0/8", "3.0.0.0/8", "4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9", "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11", "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4", "193.138.218.74/32", "::/0" ]
+
+
+mullvadIPRange :: IPRange
+mullvadIPRange = "10.64.0.0/10"
+
+
 createConfig :: PeerInfo -> ServerInfo -> Text
 createConfig PeerInfo{..} ServerInfo{..} =
+  let
+    allowedIPs = Set.insert mullvadIPRange publicIPRanges
+
+    serializeIPs :: Set IPRange -> Text
+    serializeIPs ips =
+      Set.foldr packIP "" ips
+
+    packIP :: IPRange -> Text -> Text
+    packIP ip ips
+      | Text.null ips = fromIPRange ip <> ips
+      | otherwise     = fromIPRange ip <> ", " <> ips
+
+    fromIPRange :: IPRange -> Text
+    fromIPRange = pack . show
+  in
   unlines $
     [ "[Interface]"
     , "PrivateKey = " <> peerPrivateKey
@@ -146,7 +170,9 @@ createConfig PeerInfo{..} ServerInfo{..} =
     , "\n"
     , "[Peer]"
     , "PublicKey = " <> publicKey
-    , "AllowedIPs = 0.0.0.0/0,::0/0"
+    -- Explicitly send traffic for public IP ranges through the tunnel, excluding private / LAN ranges.
+    -- To send instead everything through tunnel: 0.0.0.0/0,::0/0
+    , "AllowedIPs = " <> serializeIPs allowedIPs
     , "Endpoint = " <> ipv4AddrIn <> ":51820"
     ]
 

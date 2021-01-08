ea7c4de98d442a5ed5851ced3b407ae7ad6aeff2
sandydoo <hey@sandydoo.me>
2021-01-08 16:53:45 +0300

    Align code for readability

--- ProcessConfigs.hs
+++ ProcessConfigs.hs
@@ -53,20 +53,20 @@ countryEmojis =
 
 data ServerInfo =
   ServerInfo
-    { hostname :: Text
-    , countryCode :: Text
-    , countryName :: Text
-    , cityCode :: Text
-    , cityName :: Text
-    , active :: Bool
-    , owned :: Bool
-    , provider :: Text
-    , ipv4AddrIn :: Text
-    , ipv6AddrIn :: Text
-    , serverType :: Text
-    , publicKey :: Text
+    { hostname     :: Text
+    , countryCode  :: Text
+    , countryName  :: Text
+    , cityCode     :: Text
+    , cityName     :: Text
+    , active       :: Bool
+    , owned        :: Bool
+    , provider     :: Text
+    , ipv4AddrIn   :: Text
+    , ipv6AddrIn   :: Text
+    , serverType   :: Text
+    , publicKey    :: Text
     , multihopPort :: Int
-    , socksName :: Text
+    , socksName    :: Text
     } deriving (Generic, Show)
 
 
@@ -95,12 +95,12 @@ instance FromJSON ServerInfo where
 
 data PeerInfo =
   PeerInfo
-    { peerName :: Text
-    , peerPublicKey :: Text
+    { peerName       :: Text
+    , peerPublicKey  :: Text
     , peerPrivateKey :: Text
-    , peerIpv4Addr :: Text
-    , peerIpv6Addr :: Text
-    , peerPorts :: [Int]
+    , peerIpv4Addr   :: Text
+    , peerIpv6Addr   :: Text
+    , peerPorts      :: [Int]
     } deriving (Generic, Show)
 
 
@@ -138,7 +138,15 @@ isPreferredCountryCode code = preferredCountryCodes ^? ix code
 
 
 publicIPRanges :: Set IPRange
-publicIPRanges = Set.fromList [ "1.0.0.0/8", "2.0.0.0/8", "3.0.0.0/8", "4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9", "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11", "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4", "193.138.218.74/32", "::/0" ]
+publicIPRanges =
+  Set.fromList
+    [ "1.0.0.0/8", "2.0.0.0/8", "3.0.0.0/8", "4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4"
+    , "32.0.0.0/3" , "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11"
+    , "172.64.0.0/10", "172.128.0.0/9" , "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11"
+    , "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10"
+    , "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4", "193.138.218.74/32"
+    , "::/0"
+    ]
 
 
 mullvadIPRange :: IPRange

be396bde20cac435c880b738eac1297a6491aaea
sandydoo <hey@sandydoo.me>
2021-10-17 22:09:59 +0300

    Update nix flake
    Set up the dev environment.
    
    Based on https://github.com/srid/haskell-template/blob/d2956c03b77b13d986591424a2fd47619f44d812/flake.nix

--- flake.nix
+++ flake.nix
@@ -4,12 +4,39 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-   outputs = { self, nixpkgs, flake-utils }:
-		flake-utils.lib.eachSystem ["x86_64-darwin" "x86_64-linux"]
-		  (system:
-        let pkgs = import nixpkgs { inherit system; }; in
-        {
-          defaultPackage = pkgs.haskellPackages.callPackage ./default.nix {};
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowBroken = true;
+        };
+
+        hlib = pkgs.haskell.lib;
+        hpkgs = pkgs.haskellPackages;
+
+        project = { returnShellEnv }: hpkgs.developPackage {
+          name = "MullvadConfigs";
+          root = ./.;
+          withHoogle = false;
+
+          # Allow a shell environment to be requested
+          inherit returnShellEnv;
+
+          modifier = drv: hlib.addBuildTools drv (with hpkgs;
+            [
+              cabal-fmt
+              cabal-install
+              ghcid
+              haskell-language-server
+              ormolu
+              pkgs.nixpkgs-fmt
+            ]);
+        };
+      in
+      {
+        defaultPackage = project { returnShellEnv = false; };
+        devShell = project { returnShellEnv = true; };
+      }
+    );
 }

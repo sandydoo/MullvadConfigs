f976add68d6e231ca9261a84e485e8859a25207d
Sander <hey@sandydoo.me>
2024-01-02 23:31:49 +0400

    Bump flake dependencies

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Generate Wireguard configurations for Mullvad";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:

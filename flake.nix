df67f30a9cf895c67303dd2032274dbe229a92d3
Sander <hey@sandydoo.me>
2025-02-14 17:49:57 +0400

    flake: bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Generate Wireguard configurations for Mullvad";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:

65bed42b932807d1a89b4f94398846ebfb805d36
Sander <hey@sandydoo.me>
2025-07-31 21:31:34 +0200

    bump deps

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "Generate Wireguard configurations for Mullvad";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -15,30 +21,42 @@
         hlib = pkgs.haskell.lib;
         hpkgs = pkgs.haskellPackages;
 
-        project = { devMode, returnShellEnv }: hpkgs.developPackage {
-          name = "MullvadConfigs";
-          root = ./.;
-          withHoogle = false;
+        project =
+          { devMode, returnShellEnv }:
+          hpkgs.developPackage {
+            name = "MullvadConfigs";
+            root = ./.;
+            withHoogle = false;
 
-          # Allow a shell environment to be requested
-          inherit returnShellEnv;
+            # Allow a shell environment to be requested
+            inherit returnShellEnv;
 
-          cabal2nixOptions = if devMode then "--flag=dev" else "";
+            cabal2nixOptions = if devMode then "--flag=dev" else "";
 
-          modifier = drv: hlib.addBuildTools drv (with hpkgs;
-            [
-              cabal-fmt
-              cabal-install
-              ghcid
-              haskell-language-server
-              fourmolu
-              pkgs.nixpkgs-fmt
-            ]);
-        };
+            modifier =
+              drv:
+              hlib.addBuildTools drv (
+                with hpkgs;
+                [
+                  cabal-fmt
+                  cabal-install
+                  ghcid
+                  haskell-language-server
+                  fourmolu
+                  pkgs.nixpkgs-fmt
+                ]
+              );
+          };
       in
       {
-        packages.default = project { devMode = false; returnShellEnv = false; };
-        devShells.default = project { devMode = true; returnShellEnv = true; };
+        packages.default = project {
+          devMode = false;
+          returnShellEnv = false;
+        };
+        devShells.default = project {
+          devMode = true;
+          returnShellEnv = true;
+        };
       }
     );
 }
